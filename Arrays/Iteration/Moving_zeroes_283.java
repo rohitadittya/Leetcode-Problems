3e96a8a274c1434f53aef472e72e80b27f7edbe9
Rohit Adittya Thumati <rohitadittya@gmail.com>
2026-01-20 16:17:29 -0500

    Day 1 - Arrays - Iteration Pattern - 4 Probs

---
+++ Arrays/Iteration/Moving_zeroes_283.java
@@ -0,0 +1,51 @@
+package Arrays.Iteration;
+
+/*
+    Day 1 | Problem 2
+    LC 283. Move Zeroes | Easy
+    https://leetcode.com/problems/move-zeroes/description/
+
+    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+    Note that you must do this in-place without making a copy of the array.
+
+    
+
+    Example 1:
+
+    Input: nums = [0,1,0,3,12]
+    Output: [1,3,12,0,0]
+    Example 2:
+
+    Input: nums = [0]
+    Output: [0]
+ */
+public class Moving_zeroes_283 {
+    public void moveZeroes(int[] nums) {
+        int zeroPtr = 0, numPtr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            /*
+            if (nums[zeroPtr] != 0) {
+                zeroPtr++;
+            }
+            if (nums[numPtr] == 0) {
+                numPtr++;
+            }
+            else if (zeroPtr > numPtr) {
+                numPtr = zeroPtr;
+            }
+            
+            if (nums[zeroPtr] == 0 && nums[numPtr] !=0) {
+                nums[zeroPtr] = nums[numPtr];
+                nums[numPtr] = 0;
+            }
+             */
+            if (nums[i] != 0) {
+                int temp = nums[zeroPtr];
+                nums[zeroPtr] = nums[i];
+                nums[i] = temp;
+                zeroPtr++;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+
+# LeetCode Practice
+
+## Table of Contents
+- [Arrays](#arrays)
+
+
+
+## Arrays
+
+### Iteration
+
+#### Easy
+- [Moving Zeroes](./Arrays/Iteration/Moving_zeroes_283.java)
+
+#### Medium
+- [Two Sum](./Arrays/Iteration/Two_Sum_II_167.java)
+- [Best Time to Buy and Sell Stock](./Arrays/Iteration/BestTimeToBuyStocks_122.java)
+- [Rotate Array By K](./Arrays/Iteration/ArrayRotateByK_189.java)
+
+---
+
